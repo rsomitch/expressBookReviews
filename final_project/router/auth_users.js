6dfbeeb5880a1f34e5e339ed325838218dc62dd2
Mahershala McTrevors <51538280+rsomitch@users.noreply.github.com>
2025-01-11 05:26:44 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -36,15 +36,31 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   const user = req.session.authorization.username;
-  const review = req.body.review; // string
+  const review = req.body.review; 
   const isbn = req.params.isbn;
   if (!review) {
-    res.status(400).json({ message: "Review is empty!" });
+    res.status(400).json({ message: "No Reviews" });
   } else {
     books[isbn].reviews[user] = review;
-    res.status(200).json({ message: "Book review updated." });
+    res.status(200).json({ message: "Book reviewed." });
   }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const user = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  if (!books[isbn]) {
+    res.status(400).json({ message: "not an ISBN." });
+  } else if (!books[isbn].reviews[user]) {
+    res
+      .status(400)
+      .json({ message: "Not reviewed."});
+  } else {
+    delete books[isbn].reviews[user];
+    res.status(200).json({ message: "Review deleted." });
+  }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
